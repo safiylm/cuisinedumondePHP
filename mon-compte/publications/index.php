3900f8361ea3fbfcfd8f0cb6f2f273958005eec5
safiylm <121391404+safiylm@users.noreply.github.com>
2023-10-19 13:26:50 +0200

    itération de mes recettes

--- mon-compte/publications/index.php
+++ mon-compte/publications/index.php
@@ -1,60 +1,47 @@
-<link rel="stylesheet" href='./publications/publication.css'>
 
 <?php
 $json_object = file_get_contents("../data/recette.json");
 $tab = json_decode($json_object, true);
 ?>
 
-
 <div class="main">
     <div class="flex-container">
-
         <?php
 
-        //determiné les recettes de l'utilisateur
-        $tab_mes_recettes = [];
-        $j=0;
-        for ($i = 0; $i < count($tab["sitecuisine"]["liste_recette"]['recette']); $i++) {
-            if ($tab["sitecuisine"]["liste_recette"]['recette'][$i]['auteur'] == $_SESSION["utilisateur"]['email']) {
-                $tab_mes_recettes[$j] = $i;
-                $j++; 
-            }
-        }
-        if ($j == 0) {
-            echo "</br></br> <p> Vous n'avez ajouté aucune recette! </p>";
-        }
-        for ($i = 0; $i < $j; $i++) {
+        $num = 0;
+        foreach ($tab["sitecuisine"]["liste_recette"]['recette'] as $recette) {
 
-            $recette = $tab["sitecuisine"]["liste_recette"]['recette'][$i];
-
-            $photoo = $recette["image"];
-            $url = '../Photos/' . $photoo;
-
+            if ($recette['auteur'] == $_SESSION['utilisateur']['email']) {
+                $photoo = $recette["image"];
+                $url = '../Photos/' . $photoo;
         ?>
 
-            <div class="flex-item">
-                <div class="element">
-                    <div class="div-img">
+                <div class="flex-item">
+                    <div class="element">
+                        <div class="div-img">
 
-                        <?php if (is_file($url)) { ?>
-                            <img src='../Photos/<?php echo $photoo; ?>' class="img-thumbnail" />
-                        <?php  } else { ?>
-                            <img src='<?php echo  $photoo; ?>' class="img-thumbnail" />
-                        <?php } ?>
-                    </div>
-                    <div class="div-titre">
-                        <a class="a-titre" href='../Recette/index.php?idRecette=<?php echo $i; ?>' >
-                            <?php echo $recette['titre']; ?>
-                        </a>
+                            <?php if (is_file($url)) { ?>
+                                <img src='../Photos/<?php echo $photoo; ?>' class="img-thumbnail" />
+                            <?php  } else { ?>
+                                <img src='<?php echo  $photoo; ?>' class="img-thumbnail" />
+                            <?php } ?>
+                        </div>
+                        <div class="div-titre">
+                        <a class="a-titre" href='../Recette/index.php?idRecette=<?php echo $num; ?>'>
+                                <?php echo $recette['titre']; ?>
+                            </a> 
+                            
+                            <a class="a-titre" href='../modifier-recette/index.php?idRecette=<?php echo $num; ?>'>
+                               Modifier
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
+        <?php
 
-
-        <?php  // fin is_file($url) == non 
-        } // fin foreach recette 
-
+            }
+            $num++;
+        }
         ?>
-
     </div><!-- fin div flex container -->
 </div><!-- fin div main -->
