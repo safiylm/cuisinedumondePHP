77eae46fbb908a83d04aca74bc503e37b7c3e04b
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-11 11:39:34 +0100

    Update categorie.php

--- categorie/categorie.php
+++ categorie/categorie.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>Catégorie </title>
-    <link rel="stylesheet" href='../css/publication.css'>
-    <link rel="stylesheet" href='../css/home.css'>
+    <link rel="stylesheet" href='../css/publication.scss'>
+    <link rel="stylesheet" href='../css/home.scss'>
     <link rel="stylesheet" href='../css/nav.css'>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -42,7 +42,7 @@ if (file_exists('../data/recette-utilisateur.xml')) {
 
     <div class="main">
 
-        <h1 style='text-align:center;'><?php echo $_GET['nom']; ?></h1>
+        <h1 style='text-align:center; margin : 30px auto; text-decoration:underline;'><?php echo $_GET['nom']; ?></h1>
 
         <div class="flex-container">
             <script>
