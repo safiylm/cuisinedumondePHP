b0b7b5baeb469458024e6894c817b6e7006a32f0
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-19 16:03:12 +0100

    changement after deploy

--- categorie/index.php
+++ categorie/index.php
@@ -2,14 +2,20 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <title>Catégorie</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/body.css" />
+    <meta charset="UTF-8">
+    <title>Catégorie</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
+    <link rel="stylesheet" href="../css/body.css" />
     <link rel="stylesheet" href="../css/publication.scss" />
     <link rel="stylesheet" href="../css/categorie.scss" />
     <link rel="stylesheet" href='../css/home.scss'>
     <link rel="stylesheet" href='../css/nav.css'>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+
 </head>
 
 <?php
@@ -26,6 +32,9 @@ if (file_exists('../data/recette-utilisateur.xml')) {
 }
 
 ?>
+
+
+
 
 <body>
 
