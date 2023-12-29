1779b2de43b3c8a1fa1ded972215f4ab99ba77b1
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-29 22:00:57 +0100

    update categorie for better deployement

--- categorie/categorie.php
+++ categorie/categorie.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
@@ -15,7 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <style>
+
+<style>
     .div-categorie h3 {
         font-size: 2em;
         color: darkslategray !important;
@@ -23,7 +24,6 @@
         padding: 15px ;
     }
 </style>
-
 </head>
 
 <?php
