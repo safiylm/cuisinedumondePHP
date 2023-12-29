dd50b4ea35c939acfa3e4467640ff4c00144080c
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-29 21:59:26 +0100

    update auth for better deployment

--- auth/connexion.php
+++ auth/connexion.php
@@ -4,25 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Connexion</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
+    <link rel="stylesheet" href="../css/nav.css" />
     <link rel="stylesheet" href="../css/body.css" />
-    <link rel="stylesheet" href="../css/auth.scss" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="../css/auth.css" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </head>
 
+<body>
 <?php
-
 include("../navigation/index.php");
 ?>
 
 
 
-<body>
+
     <div class="container">
-        <img id="image-left" src="../Photos/auth.jpg" />
+        <img id="image-left" src="../Photos/auth.jpg" style="max-width:50vw; max-height:100vh" />
         <div id="div-right">
             <h2>Login</h2>
             <form action="post-connexion.php" method="post">
@@ -35,7 +36,7 @@ include("../navigation/index.php");
                     <input class="btnSubmit" type="submit" value="Se connecter " />
                     <a href='inscription.php'> S'inscrire </a>
                     <a href='mot-de-passe-oublie/index.php'> Mot de passe oublié ?  </a>
-</div>
+                </div>
 
                 <div class="info">
                     <?php if($_GET['erreur']=="password"){
@@ -56,7 +57,9 @@ include("../navigation/index.php");
         </div>
 
 
-    </div> <!-- fin div containerC -->
+    </div> 
+
+
 </body>
 
 </html>
--- auth/inscription.php
+++ auth/inscription.php
@@ -2,11 +2,14 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <title>Inscription</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/auth.scss" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../css/nav.css" />
+    <link rel="stylesheet" href="../css/auth.css" />
+    <link rel="stylesheet" href="../css/body.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </head>
@@ -19,7 +22,7 @@
   ?>
 
   <div class="container">
-    <img id="image-left" src="../Photos/auth.jpg" />
+        <img id="image-left" src="../Photos/auth.jpg" style="max-width:50vw; max-height:100vh" />
     <div id="div-right">
 
       <h2>Inscription</h2>
