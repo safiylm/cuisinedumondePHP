8e9c10396b389bdea30fc098921732afcd416b55
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-24 22:37:03 +0100

    update nac in home page

--- accueil/index.php
+++ accueil/index.php
@@ -30,10 +30,6 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-                    </li>
-
-                    <li class="nav-item">
                         <a class="nav-link" href="./categorie/index.php">Categorie</a>
                     </li>
                     <?php if (empty($_SESSION['utilisateur']['email'])) { ?>
